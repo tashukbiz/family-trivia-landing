8d938b5d674815c27e784f278f6651ec8cfbf276
tashuk <tashukwork@gmail.com>
2025-11-21 18:37:14 +1100

    docs output

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: 'export',
+  distDir: 'docs', // Output static build to /docs
   images: {
     unoptimized: true,
   },
