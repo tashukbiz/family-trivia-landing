5ea251c6b5fe96a44c45dbdf5f5707706b84a98d
tashuk <tashukwork@gmail.com>
2025-11-25 17:19:57 +1100

    Deploy static site

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  distDir: 'docs', // Output static build to /docs
+  distDir: 'out', // Output static build to /docs
   images: {
     unoptimized: true,
   },
